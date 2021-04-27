706c9a868e12745abed3ab61a4e22b5954f8b05a
Christopher Johnston <cjohnston@connected.io>
2021-04-27 14:43:55 -0400

    Normal class extracted

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -10,22 +10,43 @@ class GildedRose
   def tick
     case name
     when 'NORMAL ITEM'
-      return normal_tick
+      @item = Normal.new(name, quality, days_remaining)
+      @item.tick
     when 'Aged Brie'
-      return aged_brie_tick
+      aged_brie_tick
     when 'Sulfuras, Hand of Ragnaros'
-      return sulfuras_tick
+      sulfuras_tick
     when 'Backstage passes to a TAFKAL80ETC concert'
-      return backstage_tick
+      backstage_tick
     end
   end
 
-  def normal_tick
-    @days_remaining -= 1
-    return if quality <= 0
+  def quality
+    return @item.quality if @item
+    @quality
+  end
 
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
+  def days_remaining
+    return @item.days_remaining if @item
+    @days_remaining
+  end
+
+  class Normal
+    attr_reader :name, :quality, :days_remaining
+
+    def initialize(name, quality, days_remaining)
+      @name = name
+      @quality = quality
+      @days_remaining = days_remaining
+    end
+
+    def tick
+      @days_remaining -= 1
+      return if quality <= 0
+
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
   end
 
   def aged_brie_tick
