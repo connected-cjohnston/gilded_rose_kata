30092b830780306565ac7d71d87cea27a2935aee
Chris Johnston <cjohnston@connected.io>
2022-12-16 11:50:32 -0500

    normal item refactored

--- app/gilded_rose.rb
+++ app/gilded_rose.rb
@@ -1,7 +1,9 @@
 class GildedRose
   def update_quality(items)
     items.each do |item|
-      if item.name == 'Conjured Mana Cake'
+      if item.name == 'NORMAL ITEM'
+        handle_normal_item(item)
+      elsif item.name == 'Conjured Mana Cake'
         handle_conjured_mana_item(item)
       else
         if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
@@ -49,6 +51,15 @@ class GildedRose
         end
       end
     end
+  end
+
+  def handle_normal_item(item)
+    item.sell_in -= 1
+
+    item.quality = 0 and return if item.quality == 0
+
+    item.quality -= 1
+    item.quality -= 1 if item.sell_in < 0
   end
 
   def handle_conjured_mana_item(item)
